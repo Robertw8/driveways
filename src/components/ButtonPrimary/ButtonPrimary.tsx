e4383c9e642a5f3d48a4a200f6357ff6d5d5e44d
Robertw8 <oniedgaf@gmail.com>
2024-02-02 19:49:08 +0200

    fix ButtonPrimary styles

--- src/components/ButtonPrimary/ButtonPrimary.tsx
+++ src/components/ButtonPrimary/ButtonPrimary.tsx
@@ -1,4 +1,5 @@
-import { Button, ButtonProps } from 'antd';
+import { Button, type ButtonProps } from 'antd';
+import clsx from 'clsx';
 
 interface ButtonPrimaryProps extends ButtonProps {
   text: string;
@@ -10,14 +11,21 @@ const ButtonPrimary: React.FC<ButtonPrimaryProps> = ({
   onClick,
   htmlType,
   className,
+  loading,
 }) => {
   return (
     <Button
-      className={`w-full h-full font-semibold text-xs md:text-sm bg-blue-primary capitalize text-white font-manrope
-     hover:bg-blue-secondary hover:text-white focus:bg-blue-secondary focus:text-white ${className}`}
+      className={clsx(
+        `w-full h-full font-semibold text-xs md:text-sm capitalize font-manrope
+     `,
+        className
+          ? className
+          : 'text-white bg-blue-primary hover:bg-blue-secondary hover:text-white focus:bg-blue-secondary focus:text-white'
+      )}
       type={type}
       onClick={onClick}
       htmlType={htmlType}
+      loading={loading}
     >
       {text}
     </Button>
