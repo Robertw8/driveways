5bdeb640bf66aa4c5806fa2558a0808e21d33f5d
Robertw8 <oniedgaf@gmail.com>
2024-02-04 18:42:14 +0200

    add handling empty cars list

--- src/components/Catalog/CarsList/CarsList.tsx
+++ src/components/Catalog/CarsList/CarsList.tsx
@@ -1,15 +1,17 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { useCatalog } from '../../../hooks';
+import { useCatalog, useNotFoundTimeout } from '../../../hooks';
 
 import { CarsItem } from '..';
-import { getCatalog } from '../../../redux/catalog';
+import { Empty } from 'antd';
+
+import { getCatalog, setPaginationEnabled } from '../../../redux/catalog';
 import { getFavorites } from '../../../redux/favorites';
 import type { AppDispatch } from '../../../redux';
 import './tailwind.css';
 
 const CarsList: React.FC = () => {
-  const { cars, filters } = useCatalog();
+  const { cars, filters, isLoading } = useCatalog();
   const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
@@ -18,17 +20,35 @@ const CarsList: React.FC = () => {
     dispatch(
       getCatalog({
         search: filters.search,
-        rentalPrice: filters.rentalPrice,
       })
     );
-  }, [dispatch, filters.search]);
+  }, [dispatch, filters.search, filters.rentalPrice]);
+
+  const filteredCars = cars.filter(car =>
+    filters.rentalPrice ? Number(car.rentalPrice) <= filters.rentalPrice : car
+  );
+
+  useEffect(() => {
+    if (filteredCars.length < 12) dispatch(setPaginationEnabled(false));
+  }, [dispatch, filteredCars]);
+
+  const notFound = useNotFoundTimeout(filteredCars, isLoading);
 
   return (
-    <ul className="cars-list">
-      {cars.map((car, index) => (
-        <CarsItem car={car} key={index} />
-      ))}
-    </ul>
+    <>
+      {notFound && (
+        <Empty
+          image={Empty.PRESENTED_IMAGE_SIMPLE}
+          description="No cars found"
+        />
+      )}
+
+      <ul className="cars-list">
+        {filteredCars.map(car => (
+          <CarsItem car={car} key={car.car_id} />
+        ))}
+      </ul>
+    </>
   );
 };
 
