621557b668791c722821d92f2cf504eb804803f0
Robertw8 <oniedgaf@gmail.com>
2024-02-04 18:41:39 +0200

    add hook for not found items

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,3 @@
 export { default as useCatalog } from './useCatalog';
 export { default as useFavorites } from './useFavorites';
+export { default as useNotFoundTimeout } from './useNotFoundTimeout';
