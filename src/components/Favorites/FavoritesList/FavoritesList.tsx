6d790d9f17d61c2dde1029778877260bcc167b25
Robertw8 <oniedgaf@gmail.com>
2024-02-07 23:32:31 +0200

    remove async logic

--- src/components/Favorites/FavoritesList/FavoritesList.tsx
+++ src/components/Favorites/FavoritesList/FavoritesList.tsx
@@ -1,41 +1,27 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { useFavorites, useNotFoundTimeout } from '../../../hooks';
+import { useFavorites } from '../../../hooks';
 
-import { CarsItem, Loader } from '../..';
+import { CarsItem } from '../..';
 import { Empty } from 'antd';
-
 import '../../Catalog/tailwind.css';
-import { getFavorites } from '../../../redux/favorites';
-import type { AppDispatch } from '../../../redux';
 
 const FavoritesList: React.FC = () => {
-  const { cars, isLoading } = useFavorites();
-  const dispatch = useDispatch<AppDispatch>();
-  const notFound = useNotFoundTimeout(cars, isLoading);
+  const { cars } = useFavorites();
 
-  useEffect(() => {
-    dispatch(getFavorites({}));
-  }, [dispatch]);
+  if (!cars.length) {
+    return (
+      <Empty
+        image={Empty.PRESENTED_IMAGE_SIMPLE}
+        description="No favorite cars found"
+      />
+    );
+  }
 
   return (
-    <>
-      {notFound && (
-        <Empty
-          image={Empty.PRESENTED_IMAGE_SIMPLE}
-          description="No favorite cars found"
-        />
-      )}
-      {!isLoading ? (
-        <ul className="cars-list">
-          {cars.map(car => (
-            <CarsItem car={car} key={car.car_id} />
-          ))}
-        </ul>
-      ) : (
-        <Loader />
-      )}
-    </>
+    <ul className="cars-list">
+      {cars.map(car => (
+        <CarsItem car={car} key={car.car_id} />
+      ))}
+    </ul>
   );
 };
 
